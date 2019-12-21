3e51e344d0b08459cf68142a4b61d4d0c8436b74
Caleb Fahlgren <cfahlgren1@gmail.com>
2019-12-21 14:20:57 -0600

    changed database settings

--- settings/dev.py
+++ settings/dev.py
@@ -1,1 +1,25 @@
 from .base import *
+
+INSTALLED_APPS += [
+    'courts',
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+DATABASES = {
+        'default': {
+        'ENGINE': 'djongo',
+        'ENFORCE_SCHEMA': False,
+        'NAME': 'test_Cluster0',
+        'HOST': 'mongodb://admin:archlinux@cluster0-shard-00-00-dh4kn.mongodb.net:27017,cluster0-shard-00-01-dh4kn.mongodb.net:27017,cluster0-shard-00-02-dh4kn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+    }
+}
