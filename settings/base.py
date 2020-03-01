824b2cc95e4db51a9d4d71daea9059a4a44fb376
Castin Masikah <browncastin@gmail.com>
2020-03-01 11:35:01 +0300

    Disable Postgres

--- settings/base.py
+++ settings/base.py
@@ -141,4 +141,4 @@ import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(),databases=False)
