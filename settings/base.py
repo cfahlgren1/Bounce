c1799223afe5bb8a559510728329547e3918851b
Castin Masikah <browncastin@gmail.com>
2020-03-02 07:01:15 +0300

    ENV

--- settings/base.py
+++ settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^#0a1!ujq3c!i6rzt!z1@174bo5&-^tk-bv#8)dz6&zj3l9cl9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -92,7 +92,6 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
