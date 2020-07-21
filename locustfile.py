aba9673cfb0c7fa3160ceb02de6ad01eda167f37
Caleb Fahlgren <cfahlgren1@gmail.com>
2020-07-20 20:07:27 -0500

    added locust support for load testing

---
+++ locustfile.py
@@ -0,0 +1,10 @@
+import random
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def index_page(self):
+        self.client.get("")
+        self.client.get("courts")
