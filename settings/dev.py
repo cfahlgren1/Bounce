0907fff790692339f341e20437db02ef861bf2f2
Caleb Fahlgren <cfahlgren1@gmail.com>
2020-07-22 18:05:32 -0500

    made setttings changes

--- settings/dev.py
+++ settings/dev.py
@@ -49,7 +49,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bounce-court-finder.herokuapp.com', '127.0.0.1', 'localhost', 'bouncemap.com']
+ALLOWED_HOSTS = ['the-bounce-map.herokuapp.com', '127.0.0.1', 'localhost', 'bouncemap.com']
 
 # Application definition
 THIRD_PARTY_APPS = [
