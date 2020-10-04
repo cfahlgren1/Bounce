389322ecbb1da201f39d24f96f18e544b410ce94
Marco Richetta <marcorichetta@gmail.com>
2020-10-04 18:28:41 -0300

    Fixed typo

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$DATABASE" = "postgres" ]
 then
-    echo "Waiting for the BD..."
+    echo "Waiting for the DB..."
 
     # Using netcat to test DB connection
     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
